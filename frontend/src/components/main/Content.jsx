b8e53c7f8c7e4fec6885347b3a64fe8f0abfa4e7
Mohammad-Mancy <mohammad.mancy994@gmail.com>
2022-12-01 01:39:03 +0200

    Update content component add category bar

--- frontend/src/components/main/Content.jsx
+++ frontend/src/components/main/Content.jsx
@@ -1,10 +1,46 @@
 import React from 'react'
+import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
 
 function Content() {
+
+  // Mock server
+  const items=[
+    {id:1,
+    icon:"plattersIcon",
+    name:"Platters"},
+    {id:2,
+      icon:"plattersIcon2",
+      name:"Platters2"},
+    {id:3,
+      icon:"plattersIcon4",
+      name:"Platters4"},
+    {id:3,
+      icon:"plattersIcon5",
+      name:"Platters5"},
+    {id:3,
+      icon:"plattersIcon6",
+      name:"Platters6"},
+    {id:3,
+      icon:"plattersIcon7",
+      name:"Platters7"},
+  ]
+  // _______________________
+
   return (
     <div>
-        {/* content logic */}
-        content
+
+      {/* Category bar */}
+      <ScrollMenu scrollContainerClassName="bottom-nav">
+        {items.map(({ id,icon,name }) => (
+          <div className='category'>
+              <p>{icon}</p>
+              <div className='cat-title'>{name}</div>
+          </div>
+        ))}
+      </ScrollMenu>
+
+
+        
     </div>
   )
 }
