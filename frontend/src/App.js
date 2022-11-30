de6bbd6bff9a3d602ee69949a43106b3a2d1a1f3
Mohammad-Mancy <mohammad.mancy994@gmail.com>
2022-11-30 22:52:37 +0200

    Add main components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Main from './components/Main';
 
 function App() {
   return (
     <div className="App">
-      <h1>Restaurant menu</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Main />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ frontend/src/components/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Content from './main/Content'
+import Navbar from './main/Navbar'
+
+function Main() {
+  return (
+    <div>
+        <Navbar />
+        <Content />
+    </div>
+  )
+}
+
+export default Main
