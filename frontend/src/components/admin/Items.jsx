d1aec6ed40fef1afcb6db043e151a2b6a7c840e1
Mohammad-Mancy <mohammad.mancy994@gmail.com>
2022-12-01 15:02:12 +0200

    Update items component

--- frontend/src/components/admin/Items.jsx
+++ frontend/src/components/admin/Items.jsx
@@ -1,12 +1,76 @@
 import React from 'react'
 import NavbarComponent from '../main/NavbarComponent'
 import CMSNavbar from './CMSNavbar'
+import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
+import { RiDeleteBin5Line } from 'react-icons/ri'
+import { FaEdit } from 'react-icons/fa'
 
 function Items() {
   return (
     <div>
         <NavbarComponent />
         <CMSNavbar activeLink={'items'} />
+
+        <div className="add-item-div">
+            <Button variant="primary">Add Item</Button>
+        </div>
+
+        <Table bordered hover className='table-category'>
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Title</th>
+                    <th>Price</th>
+                    <th>Category</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Chicken Platte</td>
+                    <td>160000 <strong>L.L</strong></td>
+                    <td>Platters</td>
+                    <td>        
+                        <span >
+                            <button className='category-delete-btn'
+                            ><RiDeleteBin5Line/></button>
+                            <button className='category-edit-btn'
+                            ><FaEdit/></button>
+                        </span>    
+                    </td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Chicken Platte 2</td>
+                    <td>160000 <strong>L.L</strong></td>
+                    <td>Platters</td>
+                    <td>        
+                        <span >
+                            <button className='category-delete-btn'
+                            ><RiDeleteBin5Line/></button>
+                            <button className='category-edit-btn'
+                            ><FaEdit/></button>
+                        </span>    
+                    </td>
+                </tr>
+                <tr>
+                    <td>3</td>
+                    <td>Chicken Platte 3</td>
+                    <td>160000 <strong>L.L</strong></td>
+                    <td>Platters</td>
+                    <td>        
+                        <span >
+                            <button className='category-delete-btn'
+                            ><RiDeleteBin5Line/></button>
+                            <button className='category-edit-btn'
+                            ><FaEdit/></button>
+                        </span>    
+                    </td>
+                </tr>
+            </tbody>
+        </Table>
     </div>
   )
 }
