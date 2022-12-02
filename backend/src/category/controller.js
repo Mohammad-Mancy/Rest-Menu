714e630e0e3d8822a0788b9f46315d06771690f2
Mohammad-Mancy <mohammad.mancy994@gmail.com>
2022-12-02 20:33:43 +0200

    Apply Delete category API

--- backend/src/category/controller.js
+++ backend/src/category/controller.js
@@ -1,5 +1,6 @@
 const TOKEN_SECRET = process.env.TOKEN_SECRET || "";
 const jwt = require('jsonwebtoken');
+const Category = require('../../model/Category');
 const { addCategoryFunction } = require('./service')
 
 async function addCategory(req,res) {
@@ -19,6 +20,24 @@ async function addCategory(req,res) {
     }
   }
 
+async function deleteCategory(req,res) {
+    try {
+      // Check the token if it's Valid
+      const token = await req.headers.authorization;
+      jwt.verify(token, TOKEN_SECRET, async (err) => {
+        if (err) {
+          return res.status(401).send(err);
+        }
+        await Category.findByIdAndRemove(req.body._id);
+        res.status(204).send();//not content to send just success status
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send(error);
+    }
+}
+
 module.exports = {
-    addCategory
+    addCategory,
+    deleteCategory
 }
--- backend/src/category/index.js
+++ backend/src/category/index.js
@@ -1,8 +1,9 @@
 const { Router } = require('express');
-const { addCategory } = require('./controller')
+const { addCategory,deleteCategory } = require('./controller')
 
 const router = Router();
 
 router.post('/add', addCategory);
+router.post('/delete', deleteCategory);
 
 module.exports = router;
